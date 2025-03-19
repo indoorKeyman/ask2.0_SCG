57e926b809399f5f127da94c58c23d41b732bfd7
indoorKeyman <bonjae95@gmail.com>
2025-03-19 15:58:35 +0900

    [FIX] EIDT GLOBALCORS

--- src/main/java/com/example/SCG/CorsGlobalConfiguration.java
+++ src/main/java/com/example/SCG/CorsGlobalConfiguration.java
@@ -18,7 +18,7 @@ public class CorsGlobalConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry corsRegistry) {
                 corsRegistry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://43.201.219.118:3000/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -29,7 +29,7 @@ public class CorsGlobalConfiguration {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("http://localhost:3000"); // 프론트엔드 주소
+        corsConfig.addAllowedOrigin("http://43.201.219.118:3000/"); // 프론트엔드 주소
         corsConfig.addAllowedHeader("*"); // 모든 헤더 허용
         corsConfig.addAllowedMethod("*"); // 모든 메서드 허용 (GET, POST 등)
         corsConfig.setAllowCredentials(true); // 쿠키/인증 허용
